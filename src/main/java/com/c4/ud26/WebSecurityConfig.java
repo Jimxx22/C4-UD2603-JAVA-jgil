8ef744aaadcbd3589f79b96afbb9ae27390c3e0a
Jimxx22 <jaumegil22@gmail.com>
2022-08-18 11:48:35 +0200

    security

---
+++ src/main/java/com/c4/ud26/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.c4.ud26;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests().anyRequest().authenticated()
+		.and().formLogin().permitAll()
+		.and().httpBasic();
+	}
+}
